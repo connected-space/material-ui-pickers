bf36b32de7e7660f09082c38f2ee6616113ec191
Abbas Farid <abbasfreestyle@gmail.com>
2018-05-22 03:40:20 +0100

    removed uneccessary imports

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,5 +1,5 @@
 export { Utils } from './utils/utils';
 
 export { default as DatePicker } from './DatePicker/DatePickerWrapper';
-export { default as TimePicker } from './TimePicker/TimePickerWrapper';
-export { default as DateTimePicker } from './DateTimePicker/DateTimePickerWrapper';
+// export { default as TimePicker } from './TimePicker/TimePickerWrapper';
+// export { default as DateTimePicker } from './DateTimePicker/DateTimePickerWrapper';
